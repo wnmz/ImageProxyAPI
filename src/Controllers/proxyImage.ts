cf105ce4f07e563e687a739d6207b6bae2259113
wnmz <56759438+wnmz@users.noreply.github.com>
2022-03-08 23:06:59 +0700

    Correct logic

--- src/Controllers/proxyImage.ts
+++ src/Controllers/proxyImage.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { responseTemplate } from '../Utlis/responseTemplate';
+import request from 'request';
 
 export const proxyImage = (req: Request, res: Response) => {
     const url = req.query.url as string;
@@ -7,10 +8,11 @@ export const proxyImage = (req: Request, res: Response) => {
     const h = req.query.h as string;
 
     if (!url) return res.status(400).send('URL not specified!');
-    if (w || h) {
-        if (!Number(h) || !Number(w)) return res.status(400).send('h and w must be numbers');
-        return res.status(200).send(responseTemplate(url, w, h));
-    };
+    // if (w || h) {
+    //     if (!Number(h) || !Number(w)) return res.status(400).send('h and w must be numbers');
+    //     return res.status(200).send(responseTemplate(url, w, h));
+    // };
 
-    return res.status(200).send(responseTemplate(url));
+    return request.get(url).pipe(res);
+   // return res.status(200).send(responseTemplate(url));
 };
