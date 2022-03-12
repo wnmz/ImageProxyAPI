f42aa0079425eb4dde9f6b1f5cb80b3cb71f3d3f
wnmz <56759438+wnmz@users.noreply.github.com>
2022-03-12 13:48:01 +0700

    Update proxyImage.ts

--- src/Controllers/proxyImage.ts
+++ src/Controllers/proxyImage.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { responseTemplate } from '../Utlis/responseTemplate';
 import request from 'request';
+import { Console } from 'console';
 
 export const proxyImage = (req: Request, res: Response) => {
     const url = req.query.url as string;
@@ -8,11 +9,21 @@ export const proxyImage = (req: Request, res: Response) => {
     const h = req.query.h as string;
 
     if (!url) return res.status(400).send('URL not specified!');
+    //TODO: Resize
     // if (w || h) {
     //     if (!Number(h) || !Number(w)) return res.status(400).send('h and w must be numbers');
     //     return res.status(200).send(responseTemplate(url, w, h));
     // };
 
-    return request.get(url).pipe(res);
+    const requestSettings = {
+        url: url,
+        method: 'GET',
+        encoding: null
+    };
+
+    request(requestSettings, (error, response, body)=> {
+        res.set('Content-Type', 'image/png');
+        res.send(body);
+    });
    // return res.status(200).send(responseTemplate(url));
 };
